11b9f707e2dadad8f227bcfad20982728dd3ebbe
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-01 01:13:41 -0400

    variables added and strategy mapped

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,6 +1,19 @@
 var searchBoxContainerEL = document.querySelector(".searchBoxContainer")
 var  cityInputEl = document.querySelector(".form-input")
 
+var fullUrl =  "https://api.openweathermap.org/data/2.5/weather?q=NewYork&units=imperial&appid=d12707350df08c4703683ba822b2a53b";
+
+
+var defaultapiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+var city = "cityInputEl";
+var myApiKey = "&appid=d12707350df08c4703683ba822b2a53b";
+var farenheit = "&units=imperial"
+
+var apiUrl = defaultapiUrl + city + farenheit + myApiKey;
+
+console.log(apiUrl);
+
+
 
 
 
